fb06b085259e925a71b741db1a1d79f9728ba2eb
Jakob Sturla <jseinarsson@gmail.com>
2020-10-25 15:50:06 +0000

    Running into some sort of error when calling the function inside the function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ let story = [
     {
         id: 1,
         end: false,
-        background: "/img/first-scene.png",
+        background: "/img/scene122122.jpg",
         storyText: [
             `Once upon a time, there was a little darling damsel, whom everybody loved that looked upon her, but her old granny loved her best of all. Once she made her a hood of red samite, and since she would wear nothing else on her head, people gave her the name of "Red Hood."`, `"God help you, Red Hood!" said he.`,
         ],
@@ -17,7 +17,7 @@ let story = [
     {
         id: 11,
         end: false,
-        background: "/img/first-scene.png",
+        background: "/img/scene11.jpg",
         storyText: [
             `Red Hood ran scared from the wolf and hid behind a rock by the edge of the forest. The wolf wandered around for a long time, but soon vanished inbetween the trees.`, `As Red Hood came out from her hiding place, about to turn back home, three little pigs came walking by.`, `“Why do you hide, wee little human?” they asked.`, `“From the big, bad wolf,” she replied.`, `“Come with us, you’ll be safe,” they said, “we swear by the hairs on our chinny chin chins!”`,
         ],
@@ -32,7 +32,7 @@ let story = [
     {
         id: 12,
         end: false,
-        background: "/img/first-scene.png",
+        background: "/img/scene12.jpg",
         storyText: [
             `"Whither so early, Red Hood?"`, `"To Granny. She must have a good meal for once, and strengthen herself therewith."`, `"Where does your Granny live, Red Hood?"`, `"A good quarter of an hour's walk further in the forest, under yon three large oaks," said Red Hood.`, `The Wolf thought within himself, "This nice young damsel is a rich morsel. She will taste better than the old woman; but you must trick her cleverly, that you may catch both."`, `For a time he went by Red Hood's side. Then said he, "Red Hood! Just look! There are such pretty flowers here! Why don't you look round at them all? If you bring with you a sweet smelling nosegay to granny, it will cheer her.”`, `“It is still so early, even if I go picking flowers, I shall come to her in plenty of time," and therewith she skipped into the forest.`, `The Wolf was pleased that his ploy worked and headed down the path to Granny’s house, when suddenly, a delightful smell of cookies flooded his nose.`,
         ],
@@ -44,28 +44,14 @@ let story = [
             text: "Go to Granny", forkId: 122,
         },
     },
-    // {
-    //     id: ,
-    //     end: ,
-    //     background: ,
-    //     story: [
-
-    //     ],
-    //     choice: ,
-    //     optionA: {
-    //         text: , forkId: ,
-    //     },
-    //     optionB: {
-    //         text: , forkId: ,
-    //     },
-    // },
 ]
 
 // WRITE THE STORY TO THE HTML
 const storylineContainer = document.querySelector(".storyline");
 const storyforkContainer = document.querySelector(".storyfork");
+const storyBackground = document.getElementById("storyBG");
 
-function writeStory() {
+function writeStory(storyId) {
     let i = 0;
     const paragraph = storylineContainer.querySelector(".paragraph");
     const choice = storyforkContainer.querySelector(".choice");
@@ -73,14 +59,17 @@ function writeStory() {
     const optionB = document.getElementById("optionB");
 
     // Grab the correct ID from the Object
-    let currentStory= story.find(story => story.id === 1);
+    let currentStory= story.find(story => story.id === storyId);
     let currentStoryText = currentStory.storyText;
     //let currentStoryEnd = currentStory.end;
 
+    console.log(currentStory.storyText);
+    console.log(currentStory.storyText.length);
 
     // Push the first portion of the story
     if (i === 0) {
         paragraph.textContent = currentStoryText[i];
+        storyBackground.src = currentStory.background;
     }
 
     // Push new portion when Space is pressed
@@ -101,12 +90,31 @@ function writeStory() {
                 choice.textContent = currentStory.choice;
                 optionA.textContent = currentStory.optionA.text;
                 optionB.textContent = currentStory.optionB.text;
-            }
-        }
-    });
-}
 
-writeStory();
+                window.addEventListener('keydown', e => {
+                    console.log(e.code);
+                    if (e.code === 'ArrowLeft') {
+                        // If left trigger option A
+                        storylineContainer.classList.toggle('hidden');
+                        storyforkContainer.classList.toggle('hidden');
+                        
+                        i = 0;
+                        writeStory(currentStory.optionA.forkId);
+                    } else if (e.code === 'ArrowRight') {
+                        // If right trigger option B
+                        storylineContainer.classList.toggle('hidden');
+                        storyforkContainer.classList.toggle('hidden');
+                        
+                        i = 0;
+                        writeStory(currentStory.optionB.forkId);
+                    }
+                });
+            };
+        };
+    });
+} 
+
+writeStory(1);
 
 
 
