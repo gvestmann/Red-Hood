a5662f6d1660f99f382fb6cf941f66007dc019d7
Jakob Sturla <jseinarsson@gmail.com>
2020-10-27 15:31:39 +0000

    holy shit, it's working

--- testing/testing.js
+++ testing/testing.js
@@ -44,8 +44,9 @@ const storyContainer = document.querySelector('.story');
 let currentStory = 1;
 let storyIndex = 0;
 let storyOngoing = false;
+let storyFork = false;
 
-
+//Listen for click on start page btn
 start.addEventListener('click', e => {
     startContainer.classList.add('hidden');
     storyContainer.classList.remove('hidden');
@@ -53,6 +54,7 @@ start.addEventListener('click', e => {
     startStory(currentStory);
 });
 
+//Start story
 function startStory(storyID) {
     let storyPortion = story.find(story => story.id === storyID);
 
@@ -60,22 +62,49 @@ function startStory(storyID) {
     storyOngoing = true;
 };
 
+//Listen for keydown events if storyOngoing or StoryFork are true
 document.addEventListener('keydown', e => {
     if (storyOngoing && e.code === 'Space') {
         storyIndex++;
         continueStory(currentStory, storyIndex);
+    } else if (storyFork && e.code === 'ArrowLeft') {
+        pickFork(1);
+        storyFork = false;
+    } else if (storyFork && e.code === 'ArrowRight') {
+        pickFork(2);
+        storyFork = false;
     }
 });
 
+//Function to continue story while there's still portions left, else check for end state and then build the story fork
 function continueStory(storyID, i) {
     let storyPortion = story.find(story => story.id === storyID);
     let paragraph = storyContainer.querySelector('.paragraph');
     
     if (i < storyPortion.storyText.length) {
         paragraph.textContent = storyPortion.storyText[i];
+    } else if (storyPortion.end) {
+        paragraph.textContent = 'THE END';
     } else {
         buildFork(storyPortion.choice, storyPortion.optionA.text, storyPortion.optionB.text);
         storyOngoing = false;
+        storyFork = true;
+    }
+}
+
+function pickFork(fork) {
+    let storyPortion = story.find(story => story.id === currentStory);
+
+    if (fork === 1) {
+        console.log(`picked A/1/left which has id ${storyPortion.optionA.forkId}`);
+        currentStory = storyPortion.optionA.forkId;
+        storyIndex = 0;
+        startStory(currentStory);
+    } else if (fork === 2) {
+        console.log(`picked B/2/right which has id ${storyPortion.optionB.forkId}`);
+        currentStory = storyPortion.optionB.forkId;
+        storyIndex = 0;
+        startStory(currentStory);
     }
 }
 
